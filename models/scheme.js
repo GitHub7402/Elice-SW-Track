a3662f0d5006cc6dcb0d0c03224f5a5e5e9173b8
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-08-12 16:23:40 +0900

    Update scheme.js
    db 연결

--- models/scheme.js
+++ models/scheme.js
@@ -1,1 +1,17 @@
-const temp = "깃허브에 빈 디렉토리는 안올라가기 때문에 임시로 만듬";
+// import mongoose from "mongoose";
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      autoIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
