253c0d9cf4a1a970adef09603c518d3fb1bd69b9
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-08-12 21:46:18 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,72 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
+
+///////////
+
+const { Router } = require('express');
+const { Post } = require('../models');
+const asyncHandler = require('../utils/async-handler');
+
+const router = Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+  if (req.query.write) {
+    res.render('post/edit');
+    return;
+  }
+  
+  const page = // url 쿼리에서 page 받기, 기본값 1
+  const perPage = // url 쿼리에서 perPage 받기, 기본값 10
+  
+  const total = // 전체 게시글 수 쿼리하기
+  const posts = Post.find({}) // sort, skip, limit 사용하기
+  // total, posts 를 Promise.all 을 사용해 동시에 호출하기
+
+  const totalPage = Math.ceil(total / perPage);
+  
+  res.render('post/list', { posts, page, perPage, totalPage });
+}));
+
+router.get('/:shortId', asyncHandler(async (req, res) => {
+  const { shortId } = req.params;
+  const post = await Post.findOne({ shortId });
+  
+  if (req.query.edit) {
+    res.render('post/edit', { post });
+    return;
+  }
+  
+  res.render('post/view', { post });
+}));
+
+router.post('/', asyncHandler(async (req, res) => {
+  const { title, content } = req.body;
+  
+  if (!title || !content) {
+    throw new Error('제목과 내용을 입력 해 주세요');
+  }
+
+  const post = await Post.create({ title, content });
+  res.redirect(`/posts/${post.shortId}`);
+}));
+
+router.post('/:shortId', asyncHandler(async (req, res) => {
+  const { shortId } = req.params;
+  const { title, content } = req.body;
+  
+  if (!title || !content) {
+    throw new Error('제목과 내용을 입력 해 주세요');
+  }
+    
+  await Post.updateOne({ shortId }, {title, content });
+  res.redirect(`/posts/${shortId}`);
+}));
+
+router.delete('/:shortId', asyncHandler(async (req, res) => {
+  const { shortId } = req.params;
+  await Post.deleteOne({ shortId });
+  res.send('OK');
+}));
+
+module.exports = router;
