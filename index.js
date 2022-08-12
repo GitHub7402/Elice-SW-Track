5e93d7a583325fd3c3eab96e3c6e07c01fe75f87
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-08-12 21:54:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-const express = require("express");
-const mainRouter = require('./routes');
-const listsRouter = require('./routes/index');
-const writeRouter = require('./routes/write');
-const mysqlRouter = require('./models/scheme.js');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const dayjs = require('dayjs');
+
+const indexRouter = require('./routes');
+const postsRouter = require('./routes/posts');
+
+mongoose.connect('mongodb://localhost:27017/elice');
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB Connected');
+});
+
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.locals.formatDate = (date) => {
+  return dayjs(date).format('YYYY-MM-DD');
+}
 
-app.use("/lists", listsRouter);
-app.use("/write", writeRouter);
-app.use("/", mainRouter);
-app.use("/mysql", mysqlRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/posts', postsRouter);
+
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-    res.status(404);
-    res.send({
-        result: 'fail',
-        error: `Page not found ${req.path}`
-    });
+  next(createError(404));
 });
 
+// error handler
 app.use((err, req, res, next) => {
-    res.status(500);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.json({
-        result: 'fail',
-        error: err.message,
-    });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
+module.exports = app;
